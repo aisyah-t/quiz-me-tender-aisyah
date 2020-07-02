bb4e835ec4e354ae0cc43e8452de88188723ba1b
fai-songprasit <prapawan.songprasit@gmail.com>
2020-07-03 08:42:23 +1200

    make mordor landing background

--- client/components/Start.jsx
+++ client/components/Start.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import Question from "./Question"
 
-const Start = () => {
-    return (
-        <div className="container">
-            <p>This is the landing page!!</p>
-            <Question/>
-        </div>
-    )
+class Start extends React.Component {
+    componentDidMount() {
+        document.body.style.backgroundImage = 'url(/images/mordor.jpg)';
+        document.body.style.backgroundSize = 'cover';
+    }
+
+    componentWillUnmount() {
+        document.body.style.backgroundImage = null;
+        document.body.style.backgroundSize = null
+    }
+    
+    render() {
+        return (
+            <div>
+                <p>This is the landing page!!</p>
+                <Question/>
+            </div>
+        )
+    }
 }
 
 export default Start
