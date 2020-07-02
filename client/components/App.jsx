0902856908a644050f3bf913bafaa2761f718670
rubin-gower <rubinsemails@gmail.com>
2020-07-02 18:18:05 +1200

    added router-dom package and half done tiemr

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
+import Timer from "./timer"
+
+
 const App = () => {
   return (
+    <>
     <h1>React development has begun!</h1>
+      <Timer/>
+      </>
   )
 }
 
